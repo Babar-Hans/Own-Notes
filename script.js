b2f1397904eb1e7d7db223fafe731532fbfcf0a9
Babar Hans <babarhans34@gmail.com>
2023-03-12 17:12:59 +0500

    working on save notes

--- script.js
+++ script.js
@@ -2,25 +2,45 @@ const addBtn = document.querySelector("#addbtn");
 let main = document.querySelector("#main");
 
 
+const savenote = ()=>{
+  const notes = document.querySelectorAll(".note textarea")
+  console.log(notes)
+  const data = [];
+  notes.forEach(
+  (note)=>{
+    data.push(note.value);
+  }
+  )
+  console.log(data);
+
+}
+
+
 addBtn.addEventListener("click",()=>{
     addNote();
 })
-
 const addNote = ()=>{
     let note = document.createElement("div");
     note.classList.add("note");
     note.innerHTML = `
               <div class="toolbox">
                 <h2>Notes</h2>
-                <i class="fa-solid fa-floppy-disk"></i>
+                <i class="save fa-solid fa-floppy-disk"></i>
                 <i class="delete fa-solid fa-trash"></i>
               </div>
               <textarea></textarea>
     `;
-  
+   //delete event
     note.querySelector(".delete").addEventListener("click",()=>{
       note.remove();
     })
+
+  //save event
+    note.querySelector(".save").addEventListener("click",
+    ()=>{
+      savenote()
+    })
+
 
     main.appendChild(note);
 
